3a42eed34fbb62fd0d39df5e6fe7ebe9c19b70c0
AnthonyPageau <antopageau@hotmail.com>
2020-11-19 21:55:45 -0500

    change user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_023258) do
+ActiveRecord::Schema.define(version: 2020_11_20_024924) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "building_id"
@@ -194,8 +194,6 @@ ActiveRecord::Schema.define(version: 2020_11_18_023258) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -207,6 +205,8 @@ ActiveRecord::Schema.define(version: 2020_11_18_023258) do
     t.boolean "is_admin", default: false
     t.boolean "is_employee", default: false
     t.boolean "is_user", default: true
+    t.string "first_name", default: "New"
+    t.string "last_name", default: "User"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
