504f3a0602ade718979715fca401101a26079f2e
AnthonyPageau <antopageau@hotmail.com>
2020-11-23 17:08:02 -0500

    weather working

--- lib/ElevatorMedia.rb
+++ lib/ElevatorMedia.rb
@@ -2,8 +2,12 @@ require 'http'
 
 module ElevatorMedia
   class Streamer
-    def self.getContent
-        
+        def self.getContent
+        uri = HTTP.get('http://api.openweathermap.org/data/2.5/weather?q=Montreal,CA&appid=f2289bc932be7c633fdf061e35220125&units=metric')
+        obj = JSON.parse(uri)['main']['temp']
+        weather = "The weather is #{obj}C."
+        puts weather
+        return weather
     end
   end
 end
--- spec/elevator_media_spec.rb
+++ spec/elevator_media_spec.rb
@@ -2,5 +2,10 @@ require 'ElevatorMedia'
 
 describe 'Streamer' do 
     describe 'getContent' do
+        context "GET /weather ?" do
+            it 'return the weather in Montreal' do        
+                expect(ElevatorMedia::Streamer.getContent).to be_kind_of(String)
+            end
+        end
     end            
 end
