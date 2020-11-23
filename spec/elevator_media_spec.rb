b22930c50a8e1cd06d927e47a2bef548b8222847
AnthonyPageau <antopageau@hotmail.com>
2020-11-23 17:35:45 -0500

    adding pokemon

--- spec/elevator_media_spec.rb
+++ spec/elevator_media_spec.rb
@@ -1,11 +1,15 @@
 require 'ElevatorMedia'
 
 describe 'Streamer' do 
-    describe 'getContent' do
-        context "GET /weather ?" do
-            it 'return the weather in Montreal' do        
-                expect(ElevatorMedia::Streamer.getContent).to be_kind_of(String)
-            end
-        end
-    end            
+  describe 'getContent' do
+    context "GET /weather ?" do
+      it 'return the weather in Montreal' do        
+        expect(ElevatorMedia::Streamer.getContent("weather")).to be_kind_of(String)
+      end
+    end
+    context "Get a random pokemon image" do
+      it 'wich pokemon did you get?' do
+      end
+    end
+  end            
 end
