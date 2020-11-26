1e08610c532e7d837473f81a4f67e41525911054
AnthonyPageau <antopageau@hotmail.com>
2020-11-26 13:46:34 -0500

    api working

--- lib/ElevatorMedia.rb
+++ lib/ElevatorMedia.rb
@@ -3,21 +3,27 @@ require 'json'
 
 module ElevatorMedia
     class Streamer
-        def self.getContent(param)
+        def self.get_content(param)
             if param == "weather"
-                uri = HTTP.get('http://api.openweathermap.org/data/2.5/weather?q=Montreal,CA&appid=1bb09a963096cd5c59b74b7fa6336772&units=metric')
-                obj = JSON.parse(uri)['main']['temp']
-                weather = "The temperature in montreal is #{obj}C."
-                puts weather
-                return weather
+                get_weather
             elsif param == "pokemon"
-                id = rand(400)
-                uri = HTTP.get("https://pokeapi.co/api/v2/pokemon-form/#{id}/")
-                obj = JSON.parse(uri)['sprites']['front_default']
-                puts "Here's your awesome pokemon : #{obj}"
-                content = %[<img src="#{obj}" alt = "random pokemon" style="width:400px; height:400px">]
-                return content
+                get_pokemon
             end
+        end
+
+        def self.get_weather
+            uri = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal,CA&appid=1bb09a963096cd5c59b74b7fa6336772&units=metric")
+            obj = JSON.parse(uri)['main']['temp']
+            content = "The temperature in Montreal is #{obj}C."
+            return content
+        end
+
+        def self.get_pokemon()
+            id = rand(897)
+            uri = HTTP.get("https://pokeapi.co/api/v2/pokemon-form/#{id}")
+            obj = JSON.parse(uri)['sprites']['front_shiny']
+            content = %[<img src="#{obj}" alt = "random pokemon" style="width:400px; height:400px">]
+            return content           
         end
     end
 end
--- spec/elevator_media_spec.rb
+++ spec/elevator_media_spec.rb
@@ -1,16 +1,16 @@
 require 'ElevatorMedia'
 
 describe 'Streamer' do 
-  describe 'getContent' do
+  describe 'get_content' do
     context "whats is the weather in Montreal? ?" do
       it 'return the weather in Montreal' do        
-        expect(ElevatorMedia::Streamer.getContent("weather")).to be_kind_of(String)
+        expect(ElevatorMedia::Streamer.get_content("weather")).to be_kind_of(String)
       end
     end
     context "Get a random pokemon image" do
       it 'GOTTA CATCH THEM ALL' do
-        expect(ElevatorMedia::Streamer.getContent("pokemon")).to be_an_instance_of(String)
+        expect(ElevatorMedia::Streamer.get_content("pokemon")).to be_an_instance_of(String)
       end
     end
-  end            
+  end
 end
